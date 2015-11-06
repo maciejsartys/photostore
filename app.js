6fed11ffa2f9f98ed4b0d0afd59fef8bb711d658
Maciej Sartys <sartys.maciej@gmail.com>
2015-11-06 13:53:13 +0000

    errorhandler middleware added

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var app = express();
 
 var passport = require('passport');
 
+var errorhandler = require('errorhandler');
+
 require('./config/passport');
 
 // view engine setup
@@ -44,13 +46,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+  app.use(errorhandler());
 }
 
 // production error handler
